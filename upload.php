78157feeb6907b54155b6054c0f4e801bfd5a3fd
alexgrebell <vorotaryok@gmail.com>
2025-12-28 01:29:58 +0100

    prawie nic 4

--- upload.php
+++ upload.php
@@ -6,6 +6,10 @@
     <title>Upload</title>
 </head>
 <body bgcolor="#000000" text="#FFFFFF">
-    
+    <h1>Jesteś na stronie <?php echo $_POST["name"]; ?></h1>
+    <label for="upload">Załadować pliki</label>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="upload" id="upload">
+    </form>
 </body>
 </html>
